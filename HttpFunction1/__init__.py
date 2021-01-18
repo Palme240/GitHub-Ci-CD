3b4a77db9cf37f86ad86104f5434c61ecae527ad
Palme240 <69111687+Palme240@users.noreply.github.com>
2021-01-18 18:52:21 +0100

    Update __init__.py

--- HttpFunction1/__init__.py
+++ HttpFunction1/__init__.py
@@ -1,24 +1,24 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""An example Azure Function triggered by an HTTP get or post."""
+# __app__\http_trigger_2\__init__.py
 import logging
-
 import azure.functions as func
+from __app__.sharedcode import my_helper_functions
 
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
-    logging.info('Python HTTP trigger function processed a request.')
+    """Deal with http_trigger_2 event."""
+    logging.info("Python HTTP trigger function processed a request.")
+    logging.info(
+        "Run shared code function %s ", my_helper_functions.hello()
+    )  # pylint: disable=I0011,E1205
 
-    name = req.params.get('name')
-    if not name:
-        try:
-            req_body = req.get_json()
-        except ValueError:
-            pass
-        else:
-            name = req_body.get('name')
+    name = my_helper_functions.get_name(req)
 
     if name:
-        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
-    else:
-        return func.HttpResponse(
-             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
-             status_code=200
-        )
+        return func.HttpResponse(f"Hello from trigger 1, {name}!")
+    return func.HttpResponse(
+        "Please pass a name on the query string or in the request body",
+        status_code=400,
+    )
