7ab2b7866a9327acc6b1315d7f3350955887da2b
Palme240 <69111687+Palme240@users.noreply.github.com>
2021-01-20 14:57:51 +0100

    Update __init__.py

--- HttpFunction2/__init__.py
+++ HttpFunction2/__init__.py
@@ -7,5 +7,9 @@ client = CosmosClient(url, credential=key)
 database_name = 'testDatabase'
 try:
     database = client.create_database(database_name)
+    container = database.create_container(id=container_name, partition_key=PartitionKey(path="/productName"))
 except exceptions.CosmosResourceExistsError:
     database = client.get_database_client(database_name)
+    container = database.get_container_client(container_name)
+except exceptions.CosmosHttpResponseError:
+    raise
