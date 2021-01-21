c45967a72948e621e777c94d0ba0caca84f4be57
Palme240 <69111687+Palme240@users.noreply.github.com>
2021-01-21 17:32:09 +0100

    Update __init__.py

--- HttpFunction2/__init__.py
+++ HttpFunction2/__init__.py
@@ -1,23 +1,37 @@
-from azure.cosmos import CosmosClient, exceptions
-import os
+import logging
 
-url = os.environ['ACCOUNT_URI']
-key = os.environ['ACCOUNT_KEY']
-
-RES_GROUP=GitHubARM
-ACCT_NAME=githubcosmosdb
-
-export ACCOUNT_URI=$(az cosmosdb show --resource-group $RES_GROUP --name $ACCT_NAME --query documentEndpoint --output tsv)
-export ACCOUNT_KEY=$(az cosmosdb list-keys --resource-group $RES_GROUP --name $ACCT_NAME --query primaryMasterKey --output tsv)
+import azure.functions as func
+from ..sharedcode import cosmos
 
 
-client = CosmosClient(url, credential=key)
-database_name = 'testDatabase'
-try:
-    database = client.create_database(database_name)
-    container = database.create_container(id=container_name, partition_key=PartitionKey(path="/productName"))
-except exceptions.CosmosResourceExistsError:
-    database = client.get_database_client(database_name)
-    container = database.get_container_client(container_name)
-except exceptions.CosmosHttpResponseError:
-    raise
+def main(req: func.HttpRequest) -> func.HttpResponse:
+    logging.info('Python HTTP trigger function processed a request.')
+
+    database = req.params.get('database')
+    container = req.params.get('container')
+    if not database:
+        try:
+            req_body = req.get_json()
+        except ValueError:
+            pass
+        else:
+            database = req_body.get('database')
+    if not container:
+        try:
+            req_body = req.get_json()
+        except ValueError:
+            pass
+        else:
+            container = req_body.get('container')
+
+    if database is not None and container is not None:
+        msg = cosmos.CountDocuments(database,container)
+        return func.HttpResponse(
+            msg,
+            status_code=200
+        )
+    else:
+        return func.HttpResponse(
+             "Please pass database and container name on the query string or in the request body",
+             status_code=400
+        )
