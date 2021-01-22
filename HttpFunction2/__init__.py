9a6a07a651acae5264b504b0ae2f129b6ff4a0a4
Palme240 <69111687+Palme240@users.noreply.github.com>
2021-01-22 11:46:31 +0100

    Update __init__.py

--- HttpFunction2/__init__.py
+++ HttpFunction2/__init__.py
@@ -35,3 +35,9 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
              "Please pass database and container name on the query string or in the request body",
              status_code=400
         )
+    
+    return new ContentResult
+    {
+        StatusCode = 500,
+        Content = "Something went wrong"
+    };
